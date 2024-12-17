849fd5d1e69b79ca43901b6afdbed272f9bebb3d
bobofishbo <144623157+bobofishbo@users.noreply.github.com>
2024-12-16 21:20:10 -0500

    na

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://cmudsc.com',
-  integrations: [
-    tailwind(),
-    sitemap({
-      customPages: [
-        'https://cmudsc.com/datathon',
-        'https://cmudsc.com/anomaly',
-        'https://poker.cmudsc.com',
-      ],
-    }),
-  ],
-  compressHTML: true,
-  build: {
-    inlineStylesheets: 'auto',
-  },
-  vite: {
-    build: {
-      cssMinify: true,
-      minify: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            tsparticles: ['tsparticles'],
-            board: ['/src/pages/board.astro'],
-          },
-        },
-      },
-    },
-    ssr: {
-      noExternal: ['tsparticles'],
-    },
-  },
+  outDir: 'dist', // Ensure the output folder is 'dist'
 });
