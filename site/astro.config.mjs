e559e901e6b7309bffd212c26b980778721c5552
bobofishbo <144623157+bobofishbo@users.noreply.github.com>
2024-12-16 21:48:58 -0500

    okie

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  root: './site', // Set the root directory to 'site'
-  outDir: '../dist', // Place the build output at the root level
+  outDir: 'dist', // Output directory
 });
