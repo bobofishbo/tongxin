e82dbce5ef6a0bc29ed4c291deef0eb2a1b398e1
bobofishbo <144623157+bobofishbo@users.noreply.github.com>
2024-12-16 22:10:03 -0500

    should work?

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,41 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  outDir: 'dist', // Output directory
+  site: 'https://cmudsc.com', // Keep your production site URL
+  base: '/tongxin/', // Add this if deploying to a GitHub Pages subdirectory (replace 'tongxin' with your repo name)
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: [
+        'https://cmudsc.com/datathon',
+        'https://cmudsc.com/anomaly',
+        'https://poker.cmudsc.com',
+      ],
+    }),
+  ],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            tsparticles: ['tsparticles'],
+            board: ['/src/pages/board.astro'],
+          },
+        },
+      },
+    },
+    ssr: {
+      noExternal: ['tsparticles'],
+    },
+  },
 });
