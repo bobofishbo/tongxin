9d531c58c46522f0fad1c4214d1136206732cf0d
bobofishbo <144623157+bobofishbo@users.noreply.github.com>
2024-12-16 21:37:38 -0500

    newest

--- README.md
+++
@@ -1,3 +0,0 @@
-# CMU Data Science Club Website
-
-Remember to update ```pages/board.astro``` at the start of every semester!
--- astro.config.mjs
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  outDir: 'dist', // Ensure the output folder is 'dist'
-});
